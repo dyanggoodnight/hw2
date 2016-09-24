cad88106d67063dbc68085af2ae622ece1fe62d8
dyanggoodnight <dyang@cca.edu>
2016-09-23 23:26:14 -0700

    Update inclass-4.js

--- inclass-4.js
+++ inclass-4.js
@@ -1,1 +1,18 @@
 function setup() {
+  createCanvas(800,800);
+  background(255);
+}
+
+function draw(){
+  var a=random(255);
+  var b=random(255);
+  var c=random(255);
+  colorMode(HSB,255,0,0);
+  stroke(a,b,c);
+  fill(a,b,c);
+  var z=random(80);
+  var x=random(800);
+  var y=random(800);
+  ellipseMode(CENTER);
+  ellipse(x,y,z,z);
+}
