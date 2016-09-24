1a39c328fce79934cc9db30ff0eacd1900ba0df5
dyanggoodnight <dyang@cca.edu>
2016-09-23 22:21:29 -0700

    Update inclass-4.js

--- inclass-4.js
+++ inclass-4.js
@@ -1,12 +1,1 @@
 function setup() {
-  createCanvas(500, 500);
-  background(180);
-}  
-  function draw(){
-    stroke(0);
-    var s=0;
-    while(s<470){
-      line(s+20,20,s+20,480);
-      s=s+10;
-    }
-  }
