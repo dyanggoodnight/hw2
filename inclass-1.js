9c90d1dfe6838fad75fbd7749d0861b8337fd03f
dyanggoodnight <dyang@cca.edu>
2016-09-23 22:08:20 -0700

    Create inclass-1.js

---
+++ inclass-1.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(200,200);
+  background(255);
+  
+  var i = 0;
+  // while(i<7){
+    line(40,10,40,height-10);
+    line(70,10,70,height-10);
+    line(100,10,100,height-10);
+    line(130,10,130,height-10);
+    line(160,10,160,height-10);
+    line(190,10,190,height-10);
+    line(220,10,220,height-10);
+    // i=i+1;
+  // }
+}
