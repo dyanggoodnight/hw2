72ab819f105bb039e2d03142e7267d5022e1c2fe
dyanggoodnight <dyang@cca.edu>
2016-09-23 22:11:47 -0700

    Create inclass-3.js

---
+++ inclass-3.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(200, 200);
+  background(255);
+}  
+  function draw(){
+    stroke(0);
+triangle(10,60,60,10,110,60);
+rect(CENTER);
+rect(10,60,100,100);
+rect(70,120,20,40)
+}
