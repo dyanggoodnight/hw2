628eab1a367c6c11eb74215dff02d641c6926026
dyanggoodnight <dyang@cca.edu>
2016-09-23 22:17:49 -0700

    Update inclass-1.js

--- inclass-1.js
+++ inclass-1.js
@@ -1,16 +1,11 @@
 function setup() {
-  createCanvas(200,200);
+  createCanvas(200, 200);
   background(255);
-  
-  var i = 0;
-  // while(i<7){
-    line(40,10,40,height-10);
-    line(70,10,70,height-10);
-    line(100,10,100,height-10);
-    line(130,10,130,height-10);
-    line(160,10,160,height-10);
-    line(190,10,190,height-10);
-    line(220,10,220,height-10);
-    // i=i+1;
-  // }
+}  
+  function draw(){
+    stroke(0);
+triangle(10,60,60,10,110,60);
+rect(CENTER);
+rect(10,60,100,100);
+rect(70,120,20,40)
 }
